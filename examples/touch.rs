9436f2a4dd4b1b6fc18aeeefcd5d8ac2f828dc66
Conor Patrick <conorpp94@gmail.com>
2020-02-10 15:43:23 -0500

    slick edge detection for touch sensor, implementing button trait

--- examples/touch.rs
+++ examples/touch.rs
@@ -20,10 +20,16 @@ use hal::{
         timer::Lap,
         TouchSensor,
         touch::ButtonPins,
-        touch::ButtonState,
+        touch::profile_touch_sensing,
+    },
+    traits::{
+        buttons,
+        buttons::ButtonPress,
+        buttons::ButtonEdge,
     }
 };
 pub use hal::typestates::pin::state;
+
 
 
 #[entry]
@@ -55,67 +61,51 @@ fn main() -> ! {
     let adc = hal::Adc::from(hal.ADC0).enabled(&mut hal.pmc, &mut hal.syscon);
 
     let touch_timer = hal.ctimer.1.enabled(&mut hal.syscon);
+    let touch_sync_timer = hal.ctimer.2.enabled(&mut hal.syscon);
     let charge_pin = pins.pio1_16.into_ctimer1_mat3(&mut iocon);
 
     let mut dma = hal::Dma::from(hal.DMA0).enabled(&mut hal.syscon);
 
 
-    let mut delay_timer = Timer::new(hal.ctimer.2.enabled(&mut hal.syscon));
+    let mut delay_timer = Timer::new(hal.ctimer.0.enabled(&mut hal.syscon));
+
+    let touch_sensor = TouchSensor::new(11_900, 5, adc, touch_timer, touch_sync_timer, charge_pin, button_pins);
+    let mut touch_sensor = touch_sensor.enabled(&mut dma);
+
+    // Used to get tunning information for capacitive touch
+    if 1 == 0 {
+        let mut counts = [0u32; 3];
+        let mut times = [0u32; 128];
+        let mut results = [0u32; 128];
+        profile_touch_sensing(&mut touch_sensor, &mut delay_timer, &mut results, &mut times );
+        for i in 0 .. 128 {
+            let src = (results[i] & (0xf << 24)) >> 24;
+            let sample_num = (times[i] - 3513)/1598;
+            let button_sample_num = (times[i] - 3513)/(1598 * 3);
+
+            heprintln!("{}\t{}\t{}\t{}\t{}\t{}",times[i], i, sample_num,src, counts[(src-3) as usize], button_sample_num).unwrap();
+
+            counts[(src - 3) as usize] += 1;
+        }
+    }
+
     delay_timer.start(300.ms());
-
-    let touch_sensor = TouchSensor::new(10_000, adc, touch_timer, charge_pin, button_pins);
-
-    let mut times = [0u32; 125];
-
-    let touch_sensor = touch_sensor.enabled(&mut dma);
-    let start = delay_timer.lap().0;
-    let results = touch_sensor.get_results();
-
-    // block!(delay_timer.wait()).unwrap();
-
-    while true {
-        let mut has_zero = false;
-        for i in 0 .. 125 {
-            if results[i] != 0 {
-                if times[i] == 0 {
-                    times[i] = delay_timer.lap().0 -start;
-                }
-            }
-            else {
-                has_zero = true;
-            }
-        }
-        if !has_zero {
-            break;
-        }
-    }
-
-    for i in 0 .. 125 {
-        heprintln!("{}: {}us", i, times[i]).unwrap();
-    }
+    block!(delay_timer.wait()).unwrap();
 
     loop {
 
-        let buts = touch_sensor.get_button_state();
+        assert!( touch_sensor.count(3) >= 40 );
+        assert!( touch_sensor.count(4) >= 40 );
+        assert!( touch_sensor.count(5) >= 40 );
 
-        // assert!( touch_sensor.count(3) >= 40 );
-        // assert!( touch_sensor.count(4) >= 40 );
-        // assert!( touch_sensor.count(5) >= 40 );
-
-        //     heprintln!("{:02} {:02} {:02}",
-        //     touch_sensor.count(3),
-        //     touch_sensor.count(4),
-        //     touch_sensor.count(5),
-        // ).unwrap();
-
-        if buts.top == ButtonState::Pressed {
-            heprintln!("top").unwrap();
+        let p = touch_sensor.wait_for_any_press();
+        if  p.is_ok() {
+            heprintln!("press {:?}", p.unwrap()).unwrap();
         }
-        if buts.bot == ButtonState::Pressed {
-            heprintln!("bot").unwrap();
-        }
-        if buts.mid == ButtonState::Pressed {
-            heprintln!("mid").unwrap();
+
+        let p = touch_sensor.wait_for_any_release();
+        if  p.is_ok() {
+            heprintln!("release {:?}", p.unwrap()).unwrap();
         }
 
     }
